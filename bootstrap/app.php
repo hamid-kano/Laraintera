877777350d31753c1f8d294447b1b8324eef8be8
hk-opead <hko-1992@hotmail.com>
2026-03-28 14:46:55 +0300

    T

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,12 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(append: [
             \App\Http\Middleware\SetLocale::class,
         ]);
+        // ✅ تسجيل Spatie middlewares
+        $middleware->alias([
+            'role'       => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
